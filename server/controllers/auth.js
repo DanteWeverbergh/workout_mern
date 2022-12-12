addc9048b5a1eb81ebd31f7a76896c569592a5af
{DanteWeverbergh} <{dantweve@student.arteveldehs.be}>
2022-12-12 12:30:49 +0100

    :sparkles: regiseter user backend

---
+++ server/controllers/auth.js
@@ -0,0 +1,26 @@
+import bcrypt from 'bcrypt';
+import Jwt from 'jsonwebtoken';
+import User from '../models/User';
+
+// Register user
+export const register = async (req, res) => {
+  //
+  try {
+    const { firstName, lastName, email, password } = req.body;
+
+    const salt = await bcrypt.genSalt();
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      lastName,
+      email,
+      password: passwordHash,
+    });
+
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
